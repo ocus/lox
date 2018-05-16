8d65a27b894db28b082826f776430bf55dfb7d73
OcuS <ocus51@gmail.com>
2018-05-16 14:57:34 +0200

    Fixed author reference

--- jlox/src/main/java/fr/ocus/lox/jlox/AstSourceHtmlPrinter.java
+++ jlox/src/main/java/fr/ocus/lox/jlox/AstSourceHtmlPrinter.java
@@ -14,7 +14,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 /**
- * @author Matthieu Honel <matthieu.honel@vectaury.io>
+ * @author OcuS <ocus51@gmail.com>
  * @since 2017-08-11
  */
 public class AstSourceHtmlPrinter implements Stmt.Visitor<String>, Expr.Visitor<String> {
